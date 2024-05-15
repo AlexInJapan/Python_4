a5ab6162e94fb16a229818c44a236e90137ec961
AlexInJapan <oidov.ankhbayar.km@mynavi.jp>
2024-05-15 00:34:09 +0000

    fixing file place

---
+++ alex/day1/hello.py
@@ -0,0 +1,22 @@
+print("this is alex")
+
+math = 49
+japanese = 80
+english = 91
+sum = math+ japanese+english
+
+import sys
+args = sys.argv
+ 
+kyuuyogaku = 1100000
+
+if kyuuyogaku > 1000000:
+    zeigaku =(round((kyuuyogaku-1000000) * 0.2 + 100000))
+    sikyuugaku = (kyuuyogaku - zeigaku)
+    print(f"支給額:{sikyuugaku}、税額:{zeigaku}",end="")
+else:
+    zeigaku = round(kyuuyogaku*0.1)
+    sikyuugaku = kyuuyogaku - zeigaku
+    print(f"支給額:{sikyuugaku}、税額:{zeigaku}",end="")
+
+
